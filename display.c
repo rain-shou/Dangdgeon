7244ca539de45dddcc1a70e31bd3f919ce949c01
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-10 21:22:37 +0900

    Update display.c

--- display.c
+++ display.c
@@ -10,25 +10,50 @@ void draw_layer() {
     clear();
     for (int i = 0; i < WIDTH; ++i) {
         for (int j = 0; j < LENGTH; ++j) {
-            if (entity_layer[i][j] != ' ') {
+            if (explored_layer[i][j] == 2 && entity_layer[i][j] != ' ') {
                 mvaddch(i, j, entity_layer[i][j]);
             } else {
-                mvaddch(i, j, map_layer[i][j]);
+                if (walkable_layer[i][j] < 0 || explored_layer[i][j] == 0 ||
+                    (walkable_layer[i][j] <= 8 && explored_layer[i][j] == 1)) {
+                    mvaddch(i, j, ' ');
+                } else if (walkable_layer[i][j] <= 8 && explored_layer[i][j] == 2) {
+                    mvaddch(i, j, '.');
+                } else if (walkable_layer[i][j] == 9 ) {
+                    mvaddch(i, j, '+');
+                } else if (walkable_layer[i][j] == 10) {
+                    mvaddch(i, j, '#');
+                } else if (walkable_layer[i][j] == 11) {
+                    mvaddch(i, j, '-');
+                } else if (walkable_layer[i][j] == 12) {
+                    mvaddch(i, j, '|');
+                }
             }
         }
     }
+    mvprintw(WIDTH + 2, 0, "Layer: %3d  Level:%3d  Gold: %6d  Hp: %4d(%4d)  Str: %4d  Arm: %4d  Exp: %6d/%6d",
+             layer, player.level, player.gold, player.health, player.max_health, player.attack, player.defense, player.experience, player.level * 100);
     refresh();
 }
 
 void reveal_room() {
-    for (int i = 0; i < 9; i++) {
-        if (player_pos_x <= room_data[i].map_info.east && player_pos_x >= room_data[i].map_info.west
-            && player_pos_y <= room_data[i].map_info.south && player_pos_y >= room_data[i].map_info.north) {
-            for (int x = room_data[i].map_info.north + 1; x <= room_data[i].map_info.south - 1; ++x) {
-                for (int y = room_data[i].map_info.west + 1; y <= room_data[i].map_info.east - 1; ++y) {
-                    map_layer[x][y] = '.';
-                }
+    for (int i = 0; i < WIDTH; ++i) {
+        for (int j = 0; j < LENGTH; ++j) {
+            if (explored_layer[i][j] == 2) {
+                explored_layer[i][j] = 1;
             }
+        }
+    }
+    int n = walkable_layer[player_pos_y][player_pos_x];
+    if (n >= 0 && n < 9) {
+        for (int i = room_data[n].map_info.north; i <= room_data[n].map_info.south; ++i) {
+            for (int j = room_data[n].map_info.west; j <= room_data[n].map_info.east; ++j) {
+                explored_layer[i][j] = 2;
+            }
+        }
+    }
+    for (int i = player_pos_y - 1; i <= player_pos_y + 1; ++i) {
+        for (int j = player_pos_x - 1; j <= player_pos_x + 1; ++j) {
+            explored_layer[i][j] = 2;
         }
     }
 }
