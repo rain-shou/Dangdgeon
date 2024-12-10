e41e12b0e6beab562535a56e2fd0d2d979cb31b3
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-11 07:10:20 +0900

    Update environment_generator.c

--- environment_generator.c
+++ environment_generator.c
@@ -30,3 +30,16 @@ void generate_stairs(void) {
         }
     }
 }
+
+void generate_traps(void) {
+    int n = (int)sqrt(layer);
+    for (int i = 0; i < n;) {
+        int pos_x = (int)(random() % 99 + 1);
+        int pos_y = (int)(random() % 35 + 1);
+        if (walkable_layer[pos_y][pos_x] >= 0 && walkable_layer[pos_y][pos_x] < 9
+            && entity_layer[pos_y][pos_x] == ' ') {
+            walkable_layer[pos_y][pos_x] = 15;
+            i++;
+        }
+    }
+}
