0988955bcd226896cd524d438397e2a3bd383b8e
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-11 07:26:12 +0900

    Create script.c

---
+++ script.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "script.h"
+#include "shared.h"
+#include "dialogue.h"
+#include "game_init.h"
+
+void trigger_trap(void) {
+    int damage = (int)(random() % 10 + 1);
+    char message[20];
+    player.health -= damage;
+    add_line_to_dialogue(dialogue, "You stepped on a trap!");
+    snprintf(message, sizeof(message), "You lost %d health", damage);
+    add_line_to_dialogue(dialogue, message);
+}
+
+bool check_death(void) {
+    if (player.health <= 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void trigger_downstairs(void) {
+    save_current_layer(current_layer - 1);
+    current_layer++;
+    flag = true;
+    add_line_to_dialogue(dialogue, "You descend into the depths.");
+}
+
+void trigger_upstairs(void) {
+    save_current_layer(current_layer - 1);
+    current_layer--;
+    flag = false;
+    add_line_to_dialogue(dialogue, "You ascend toward the light.");
+}
