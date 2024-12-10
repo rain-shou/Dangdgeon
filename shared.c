1e8b1bf260780fab55beb2b2b4b936a9e273e8d9
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-11 03:42:19 +0900

    Update shared.c

--- shared.c
+++ shared.c
@@ -1,1 +1,17 @@
+#include "shared.h"
 
+char entity_layer[WIDTH][LENGTH];
+int walkable_layer[WIDTH][LENGTH];
+int explored_layer[WIDTH][LENGTH];
+int total_walkable_layer[LAYER][WIDTH][LENGTH];
+int total_explored_layer[LAYER][WIDTH][LENGTH];
+struct region_info regions[8];
+struct room_info room_data[9];
+struct region_info total_regions[LAYER][8];
+struct room_info total_room_data[LAYER][9];
+struct player_data player;
+int player_pos_x, player_pos_y;
+int layer;
+int current_layer;
+int upstairs_pos[LAYER][2];
+int downstairs_pos[LAYER][2];
