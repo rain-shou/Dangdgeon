d37da280dceff2efca7d828c22109f9670457b8b
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-11 07:25:57 +0900

    Create script.h

---
+++ script.h
@@ -0,0 +1,11 @@
+#ifndef DANGDGEON_SCRIPT_H
+#define DANGDGEON_SCRIPT_H
+
+#include <stdbool.h>
+
+void trigger_trap(void);
+bool check_death(void);
+void trigger_downstairs(void);
+void trigger_upstairs(void);
+
+#endif //DANGDGEON_SCRIPT_H
