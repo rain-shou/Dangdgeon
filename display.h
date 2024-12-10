db77c884ee83e705c4b386e8c3bbed51ea94ebd2
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-10 16:19:10 +0900

    Create display.h

---
+++ display.h
@@ -0,0 +1,11 @@
+#ifndef DANGDGEON_DISPLAY_H
+#define DANGDGEON_DISPLAY_H
+
+#include "shared.h"
+
+void draw_layer();
+void reveal_room();
+void show_start_screen();
+bool show_exit_screen();
+
+#endif //DANGDGEON_DISPLAY_H
