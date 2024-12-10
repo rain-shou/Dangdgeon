dfe1a764a930e48b5b465348eeffdb0f167c2946
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-11 03:27:43 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,11 +4,13 @@
 #include "player_control.h"
 #include "map_generator.h"
 #include "game_init.h"
-#include "display.h"
-#include "entity_generator.h"
+#include "rendering.h"
+#include "player_generator.h"
+#include "interface.h"
 
 int main(void)
 {
+    bool flag = true;
     srandom((unsigned)time(NULL));
     ncurses_init();
 
@@ -16,9 +18,19 @@ int main(void)
     initialize_game();
     while (1) {
         loop:
-        initialize_layer();
-        generate_map();
-        generate_entity();
+        if (current_layer > layer) {
+            initialize_layer();
+            generate_map();
+            generate_player();
+        } else if (current_layer <= layer) {
+            load_layer(current_layer - 1);
+            if (flag) {
+                restore_player_to_upstairs();
+            } else {
+                restore_player_to_downstairs();
+            }
+
+        }
         while (1) {
             reveal_room();
             draw_layer();
@@ -27,8 +39,16 @@ int main(void)
                 case 0:
                     break;
                 case 1:
+                    save_current_layer(current_layer - 1);
+                    current_layer++;
+                    flag = true;
                     goto loop;
                 case 2:
+                    save_current_layer(current_layer - 1);
+                    current_layer--;
+                    flag = false;
+                    goto loop;
+                case 9:
                     if (!show_exit_screen()) {
                         ncurses_cleanup();
                         exit(EXIT_SUCCESS);
