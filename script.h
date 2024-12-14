0a55a7b9ab30ebf59118783d8cec8e297a876ab6
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-15 07:20:11 +0900

    Update script.h

--- script.h
+++ script.h
@@ -7,5 +7,11 @@ void trigger_trap(void);
 bool check_death(void);
 void trigger_downstairs(void);
 void trigger_upstairs(void);
+void equip_equipment(int);
+void pickup_weapon(void);
+void pickup_armor(void);
+void pickup_gold(void);
+void pickup_potion(void);
+void drink_potion(void);
 
 #endif //DANGDGEON_SCRIPT_H
