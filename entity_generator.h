e71a153e08244fe1565f21b39dd35eeffa74fb42
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-15 07:03:47 +0900

    Create entity_generator.h

---
+++ entity_generator.h
@@ -0,0 +1,10 @@
+#ifndef DANGDGEON_ENTITY_GENERATOR_H
+#define DANGDGEON_ENTITY_GENERATOR_H
+
+void refresh_entity_layer(void);
+void generate_entity_layer(void);
+void generate_equipment(void);
+void generate_gold(void);
+void generate_potion(void);
+
+#endif //DANGDGEON_ENTITY_GENERATOR_H
