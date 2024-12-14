c57138d7d8c4d9b670a12c41d0aeccfd92fff62d
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-15 04:50:12 +0900

    Update game_init.h

--- game_init.h
+++ game_init.h
@@ -8,5 +8,6 @@ void initialize_layer();
 void initialize_player(struct player_data *player);
 void load_layer(int n);
 void save_current_layer(int n);
+void assign_equipments_to_unique_floors(void);
 
 #endif //DANGDGEON_GAME_INIT_H
