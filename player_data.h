0bb81cf644b9d0b69a525b7002f3707fc77d042a
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-15 05:01:40 +0900

    Update player_data.h

--- player_data.h
+++ player_data.h
@@ -3,15 +3,29 @@
 
 #define MAX_NAME_LENGTH 16
 
+struct bag_data {
+    int category;   // 0 for weapon, 1 for armor
+    int number;
+    char name[20];
+    bool has_equipped;
+};
+
 struct player_data {
     char name[MAX_NAME_LENGTH + 1];
     int health;
     int max_health;
     int attack;
+    int total_attack;
     int defense;
+    int total_defense;
     int experience;
     int level;
     int gold;
+    int weapon;
+    int armor;
+    int portion;
+    struct bag_data bag[16];
+    int bag_number;
 };
 
 extern struct player_data player;
