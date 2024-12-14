a0d3d00bd69ae6f26ed1d2b12512189e4c385f9c
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-15 04:44:56 +0900

    Update curses_dealer.h

--- curses_dealer.h
+++ curses_dealer.h
@@ -1,7 +1,8 @@
 #ifndef DANGDGEON_CURSES_DEALER_H
 #define DANGDGEON_CURSES_DEALER_H
 
-void ncurses_init();
-void ncurses_cleanup();
+void ncurses_init(void);
+void ncurses_cleanup(void);
+void set_terminal_size(void);
 
 #endif //DANGDGEON_CURSES_DEALER_H
