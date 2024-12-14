1f34071ffd4c7f2c4b395750ab8f2a1e6a5b8991
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-15 07:11:26 +0900

    Update interface.h

--- interface.h
+++ interface.h
@@ -7,5 +7,6 @@ void show_start_screen();
 bool show_exit_screen();
 bool show_game_over(void);
 void get_player_name(char *name, int max_length);
+void open_bag(void);
 
 #endif //DANGDGEON_INTERFACE_H
