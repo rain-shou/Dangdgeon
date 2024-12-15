4fa6dc9ad47bc3276e425cf802f8e8ffd830db1c
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-16 00:21:48 +0900

    Update player_generator.h

--- player_generator.h
+++ player_generator.h
@@ -1,8 +1,8 @@
 #ifndef DANGDGEON_PLAYER_GENERATOR_H
 #define DANGDGEON_PLAYER_GENERATOR_H
 
-void generate_player();
-void restore_player_to_upstairs();
-void restore_player_to_downstairs();
+void generate_player(void);
+void restore_player_to_upstairs(void);
+void restore_player_to_downstairs(void);
 
 #endif //DANGDGEON_PLAYER_GENERATOR_H
