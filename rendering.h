2e99559812a41c14f06b74baaa95ab3ce27c2d5a
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-16 00:23:05 +0900

    Update rendering.h

--- rendering.h
+++ rendering.h
@@ -1,7 +1,7 @@
 #ifndef DANGDGEON_RENDERING_H
 #define DANGDGEON_RENDERING_H
 
-void draw_layer();
-void reveal_room();
+void draw_layer(void);
+void reveal_room(void);
 
 #endif //DANGDGEON_RENDERING_H
