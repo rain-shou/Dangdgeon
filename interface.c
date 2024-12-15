b816e6d3fc8ad92650457689dfcf277edd5e8012
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-16 00:13:12 +0900

    Update interface.c

--- interface.c
+++ interface.c
@@ -9,7 +9,7 @@
 #define START_SCREEN_OPTION_COUNT 2
 #define EXIT_SCREEN_OPTION_COUNT 2
 
-void show_start_screen() {
+void show_start_screen(void) {
     int current_option = 0;
     const char *title[] = {
             " /$$$$$$$   /$$$$$$  /$$   /$$  /$$$$$$           ",
@@ -67,7 +67,7 @@ void show_start_screen() {
     }
 }
 
-bool show_exit_screen() {
+bool show_exit_screen(void) {
     int current_option = 0;
     const char *options[EXIT_SCREEN_OPTION_COUNT] = {"RESUME GAME", "EXIT GAME"};
 
@@ -119,8 +119,9 @@ bool show_game_over(void) {
             mvprintw(3 + i, (int)(COLS - strlen(title[i])) / 2, "%s", title[i]);
         }
         mvprintw(6 + title_lines, (int)(COLS - strlen(player.name)) / 2, "%s", player.name);
-        mvprintw(7 + title_lines, (int)(COLS - 13) / 2, "Layer: %6d", layer);
-        mvprintw(8 + title_lines, (int)(COLS - 12) / 2, "Gold: %6d", player.gold);
+        mvprintw(7 + title_lines, (int)(COLS - 13) / 2, "Level: %6d", player.level);
+        mvprintw(8 + title_lines, (int)(COLS - 13) / 2, "Layer: %6d", layer);
+        mvprintw(9 + title_lines, (int)(COLS - 12) / 2, "Gold: %6d", player.gold);
         for (int i = 0; i < START_SCREEN_OPTION_COUNT; i++) {
             if (i == current_option) {
                 mvprintw(12 + title_lines + i, (int)(COLS - strlen(options[i]) - 4) / 2, "< %s >", options[i]);
@@ -151,6 +152,7 @@ bool show_game_over(void) {
 
 void get_player_name(char *name, int max_length) {
     char buffer[128];
+    
     echo();
     curs_set(1);
     clear();
@@ -189,6 +191,60 @@ void open_bag(void) {
                 break;
             case 'q':
                 return;
+            default:
+                break;
+        }
+    }
+}
+
+bool show_game_end(void) {
+    int current_option = 0;
+    char *message1 = "You have claimed the Amulet of Yendor and escaped the treacherous dungeon.";
+    char *message2 = "Your legacy will live on forever.";
+    const char *title[] = {
+            "  ____                            _         _       _   _                 _ _ \n",
+            " / ___|___  _ __   __ _ _ __ __ _| |_ _   _| | __ _| |_(_) ___  _ __  ___| | |\n",
+            "| |   / _ \\| '_ \\ / _` | '__/ _` | __| | | | |/ _` | __| |/ _ \\| '_ \\/ __| | |\n",
+            "| |__| (_) | | | | (_| | | | (_| | |_| |_| | | (_| | |_| | (_) | | | \\__ \\_|_|\n",
+            " \\____\\___/|_| |_|\\__, |_|  \\__,_|\\__|\\__,_|_|\\__,_|\\__|_|\\___/|_| |_|___(_|_)\n",
+            "                  |___/                                                       "
+    };
+    const int title_lines = sizeof(title) / sizeof(title[0]);
+    const char *options[START_SCREEN_OPTION_COUNT] = {"RESTART", "EXIT GAME"};
+
+    for (;;) {
+        clear();
+        for (int i = 0; i < title_lines; i++) {
+            mvprintw(3 + i, (int)(COLS - strlen(title[i])) / 2, "%s", title[i]);
+        }
+        mvprintw(5 + title_lines, (int)(COLS - strlen(message1)) / 2, "%s", message1);
+        mvprintw(7 + title_lines, (int)(COLS - strlen(player.name)) / 2, "%s", player.name);
+        mvprintw(8 + title_lines, (int)(COLS - 13) / 2, "Level: %6d", player.level);
+        mvprintw(9 + title_lines, (int)(COLS - 13) / 2, "Layer: %6d", layer);
+        mvprintw(10 + title_lines, (int)(COLS - 12) / 2, "Gold: %6d", player.gold);
+        mvprintw(12 + title_lines, (int)(COLS - strlen(message2)) / 2, "%s", message2);
+        for (int i = 0; i < START_SCREEN_OPTION_COUNT; i++) {
+            if (i == current_option) {
+                mvprintw(15 + title_lines + i, (int)(COLS - strlen(options[i]) - 4) / 2, "< %s >", options[i]);
+            } else {
+                mvprintw(15 + title_lines + i, (int)(COLS - strlen(options[i])) / 2, "%s", options[i]);
+            }
+        }
+        refresh();
+        switch (getch()) {
+            case KEY_UP:
+                current_option = (current_option - 1 + START_SCREEN_OPTION_COUNT) % START_SCREEN_OPTION_COUNT;
+                break;
+            case KEY_DOWN:
+                current_option = (current_option + 1) % START_SCREEN_OPTION_COUNT;
+                break;
+            case '\n':
+                if (current_option == 0) {
+                    return true;
+                } else if (current_option == 1) {
+                    return false;
+                }
+                break;
             default:
                 break;
         }
