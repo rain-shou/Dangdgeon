9230f429829a49273ac4afe0e9df190ea89c9cc6
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-15 23:52:59 +0900

    Update environment_generator.h

--- environment_generator.h
+++ environment_generator.h
@@ -1,7 +1,7 @@
 #ifndef DANGDGEON_ENVIRONMENT_GENERATOR_H
 #define DANGDGEON_ENVIRONMENT_GENERATOR_H
 
-void generate_stairs();
+void generate_stairs(void);
 void generate_traps(void);
 
 #endif //DANGDGEON_ENVIRONMENT_GENERATOR_H
