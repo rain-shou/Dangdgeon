a3d61c3f6cd7b61717a1407bafedc1cd68f52e7b
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-17 02:06:54 +0900

    Update player_generator.c

--- player_generator.c
+++ player_generator.c
@@ -6,30 +6,29 @@
 void generate_player(void) {
     if (layer == 1) {
         for (;;) {
-            player_pos_x = (int)(random() % 99 + 1);
-            player_pos_y = (int)(random() % 35 + 1);
-            if (walkable_layer[player_pos_y][player_pos_x] >= 0 && walkable_layer[player_pos_y][player_pos_x] < 9
-                && entity_layer[player_pos_y][player_pos_x] == ' ') {
-                entity_layer[player_pos_y][player_pos_x] = '@';
+            player.pos_x = (int)(random() % 99 + 1);
+            player.pos_y = (int)(random() % 35 + 1);
+            if (walkable_layer[player.pos_y][player.pos_x] >= 0 && walkable_layer[player.pos_y][player.pos_x] < 9
+                && entity_layer[player.pos_y][player.pos_x] == ' ') {
+                entity_layer[player.pos_y][player.pos_x] = '@';
                 break;
             }
         }
     } else {
-        player_pos_x = upstairs_pos[current_layer - 1][0];
-        player_pos_y = upstairs_pos[current_layer - 1][1];
-        entity_layer[player_pos_y][player_pos_x] = '@';
-
+        player.pos_x = upstairs_pos[current_layer - 1][0];
+        player.pos_y = upstairs_pos[current_layer - 1][1];
+        entity_layer[player.pos_y][player.pos_x] = '@';
     }
 }
 
 void restore_player_to_upstairs(void) {
-    player_pos_x = upstairs_pos[current_layer - 1][0];
-    player_pos_y = upstairs_pos[current_layer - 1][1];
-    entity_layer[player_pos_y][player_pos_x] = '@';
+    player.pos_x = upstairs_pos[current_layer - 1][0];
+    player.pos_y = upstairs_pos[current_layer - 1][1];
+    entity_layer[player.pos_y][player.pos_x] = '@';
 }
 
 void restore_player_to_downstairs(void) {
-    player_pos_x = downstairs_pos[current_layer - 1][0];
-    player_pos_y = downstairs_pos[current_layer - 1][1];
-    entity_layer[player_pos_y][player_pos_x] = '@';
+    player.pos_x = downstairs_pos[current_layer - 1][0];
+    player.pos_y = downstairs_pos[current_layer - 1][1];
+    entity_layer[player.pos_y][player.pos_x] = '@';
 }
