666b4d0422a54919a68e6f3f296ce540315ef871
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-16 20:13:10 +0900

    Update environment_generator.h

--- environment_generator.h
+++ environment_generator.h
@@ -1,10 +1,7 @@
-#ifndef DANGDGEON_ENEMY_GENERATOR_H
-#define DANGDGEON_ENEMY_GENERATOR_H
+#ifndef DANGDGEON_ENVIRONMENT_GENERATOR_H
+#define DANGDGEON_ENVIRONMENT_GENERATOR_H
 
-#include "enemy_data.h"
+void generate_stairs(void);
+void generate_traps(void);
 
-void generate_current_layer_enemies(void);
-void refresh_enemy(void);
-void generate_enemy(struct enemy_data *);
-
-#endif //DANGDGEON_ENEMY_GENERATOR_H
+#endif //DANGDGEON_ENVIRONMENT_GENERATOR_H
