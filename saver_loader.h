ad80565bac4142c7fd267e25806a768995a4b8a0
rain-shou <90150609+rain-shou@users.noreply.github.com>
2024-12-17 02:09:53 +0900

    Create saver_loader.h

---
+++ saver_loader.h
@@ -0,0 +1,9 @@
+#ifndef DANGDGEON_SAVER_LOADER_H
+#define DANGDGEON_SAVER_LOADER_H
+
+#include <stdbool.h>
+
+void save_game(const char *filename);
+bool load_game(const char *filename);
+
+#endif //DANGDGEON_SAVER_LOADER_H
